ed3ebd84f55d3af6eb9244d16ed96989a325856b
G. Tuchacek <tuc18371@spengergasse.at>
2021-05-06 22:55:33 +0200

    controller

--- Owl-Ops-Webserver/Dto/TemperaturSensorDto.cs
+++ Owl-Ops-Webserver/Dto/TemperaturSensorDto.cs
@@ -1,30 +1,11 @@
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace Owl_Ops_Webserver.Dto
 {
-    public class TemperaturSensorDto
+    public record TemperaturSensorDto()
     {
-        [HttpPost("sensordata")]
-        [ProducesResponseType(StatusCodes.Status)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult SaveSensordata([FromBody] SensordataDto data)
-        {
-            Console.WriteLine($"Received temperature {data.Temperature}°  with timestamp {data.Timestamp}.");
-            return NoContent();
-        }
-
-        /// <summary>
-        /// Reagiert auf POST /api/raspberry/heartbeat und setzt den letzten empfangenen Hearbeat
-        /// im Service.
-        /// </summary>
-        [HttpPost("heartbeat")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult SetHeartbeat([FromBody] double timestamp)
-        {
-            Console.WriteLine($"Received hearbeat with timestamp {timestamp}.");
-            _hearbeatService.SetHeartbeat(timestamp);
-            return NoContent();
-        }
+        public double Value { get; set; }
+        public decimal Timestamp { get; set; }
     }
 }
