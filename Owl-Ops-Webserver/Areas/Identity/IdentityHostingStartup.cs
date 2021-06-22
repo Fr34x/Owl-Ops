81d8681df6d4429fca8bf78b2b8ec4cc348175c7
giacomo <giacomo.tuchacek@gmx.at>
2021-06-22 21:37:04 +0200

    login, register added

---
+++ Owl-Ops-Webserver/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Owl_Ops_Webserver.Model;
+
+[assembly: HostingStartup(typeof(Owl_Ops_Webserver.Areas.Identity.IdentityHostingStartup))]
+namespace Owl_Ops_Webserver.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
